c01e6e3d2ac7b9643b730829132d7051d0bce836
Dahmane Seghier <dahmane.seghier@gmail.com>
2021-07-19 17:31:37 +0100

    Added shared packaged

---
+++ resto-common/src/commands.ts
@@ -0,0 +1,5 @@
+export enum Commands {
+    SET_TABLE_STATE = 'set_table_state',
+    REQUEST_TABLES_STATE = 'request_tables_state',
+    TABLES_STATE_CHANGED = 'tables_state_changed'
+}
---
+++ resto-common/src/table-state.ts
@@ -0,0 +1,6 @@
+export enum TableState{
+    FREE = 'free',
+    BOOKED = 'booked',
+    OCCUPIED = 'occupied',
+    BOOKED_ARRIVED = 'booked-arrived'
+}
